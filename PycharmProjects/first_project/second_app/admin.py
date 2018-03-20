446bc62566da9fc2c691c3021927e238c0679194
SIVAGOPIREDDY <pkanani69@gmail.com>
2018-03-20 12:52:51 -0500

    update

--- PycharmProjects/first_project/second_app/admin.py
+++ PycharmProjects/first_project/second_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from second_app.models import User
+from second_app.models import User,details
 
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(details)
